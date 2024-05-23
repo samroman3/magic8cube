ab87b85589e931c567f793bc07453e94ad1f8e79
Samuel Roman <samroman3@gmail.com>
2024-05-23 04:09:30 -0400

    optimize performance, remove redundancies

--- 8cube/Magic8CubeView.swift
+++ 8cube/Magic8CubeView.swift
@@ -1,9 +1,10 @@
 //
-//  ContentView.swift
+//  Magic8CubeView.swift
 //  8cube
 //
 //  Created by Sam Roman on 5/22/24.
 //
+
 import SwiftUI
 import Combine
 
@@ -13,8 +14,8 @@ struct Magic8CubeView: View {
     @State private var isAnimating: Bool = false
     @State private var offset: CGFloat = 0
     @State private var showParticles: Bool = true
-    @State private var blurOpacity: Double = 0.2
     @State private var answerOpacity: Double = 0.0
+    @State private var shakeOffset: CGFloat = 0
     
     private let answers = [
         "No", "Not Likely", "Absolutely", "Definitely", "Yes", "Ask Again", "Maybe", "Try Later"
@@ -27,53 +28,16 @@ struct Magic8CubeView: View {
     var body: some View {
         VStack {
             ZStack {
-                RoundedRectangle(cornerRadius: 20)
-                                  .fill(Color.clear)
-                                  .background(
-                                      RadialGradient(
-                                          gradient: Gradient(colors: [Color(hex: "27124F"), Color(hex: "361C7D")]),
-                                          center: .center,
-                                          startRadius: 0,
-                                          endRadius: 100
-                                      )
-                                      .clipShape(RoundedRectangle(cornerRadius: 20))
-                                  )
-                                  .overlay(
-                                      RadialGradient(
-                                          gradient: Gradient(colors: [Color.white.opacity(0.2), Color.clear]),
-                                          center: .topLeading,
-                                          startRadius: 0,
-                                          endRadius: 50
-                                      )
-                                      .clipShape(RoundedRectangle(cornerRadius: 20))
-                                  )
-                                  .overlay(
-                                      RadialGradient(
-                                          gradient: Gradient(colors: [Color.clear, Color.white.opacity(0.1)]),
-                                          center: .bottomTrailing,
-                                          startRadius: -50,
-                                          endRadius: 0
-                                      )
-                                      .clipShape(RoundedRectangle(cornerRadius: 20))
-                                  )
-                                  .frame(width: 200, height: 200)
-                                  .shadow(radius: 10)
+                createBackground()
+                    .frame(width: 200, height: 200)
+                    .shadow(radius: 10)
+                    .offset(y: shakeOffset)
                 
-                ScrollView(.vertical, showsIndicators: false) {
-                    VStack(spacing: 0) {
-                        ForEach(0..<answerCount * 10, id: \.self) { index in
-                            Text(answers[index % answerCount])
-                                .foregroundColor(.white)
-                                .font(.custom("Helvetica-Bold", size: 35))
-                                .frame(width: 180, height: 50, alignment: .leading)
-                                .padding(.leading, 10)
-                        }
-                    }.opacity(0.5)
-                        .offset(y: offset)
-                        .animation(Animation.easeInOut(duration: 4).repeatCount(0, autoreverses: false))
-                }
-                .frame(width: 200, height: 200)
-                .clipShape(.rect(cornerRadius: 20))
+                createAnswerScrollView()
+                    .frame(width: 200, height: 200)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+//                    .animation(.bouncy,value: shakeOffset)
+                    .offset(y: shakeOffset)
                 
                 if showParticles {
                     CustomParticleView()
@@ -90,38 +54,89 @@ struct Magic8CubeView: View {
                     .padding(.leading, 10)
                     .animation(.easeInOut(duration: 0.4), value: answerOpacity)
                 
-                VStack {
-                    Spacer()
-                    HStack {
-                        if !isAnimating{
-                            Spacer()
-                            Button(action: {
-                                startAnimation()
-                            }) {
-                                Image(systemName: "arrow.clockwise")
-                                    .font(.body)
-                                    .foregroundColor(.white)
-                                    .padding(15)
-                                    .background(Material.ultraThinMaterial.opacity(0.4))
-                                    .clipShape(Circle())
-                                    .overlay(
-                                        Circle()
-                                            .stroke(Color.white.opacity(0.3), lineWidth: 0.2)
-                                    )
-                                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 5)
-                            }
-                            .padding(10)
-                            .clipShape(Circle())
-                            .animation(.easeInOut, value: isAnimating)
-                        }
-                    }
-                }
-                .frame(width: 200, height: 200)
-                .clipped()
+                createButton()
+                    .frame(width: 200, height: 200)
+                    .clipped()
             }
             .onAppear {
-                particleManager.createParticles() // Spread out particles initially
+                particleManager.createParticles()
                 startAnimation()
+            }
+        }
+    }
+    
+    private func createBackground() -> some View {
+        RoundedRectangle(cornerRadius: 20)
+            .fill(Color.clear)
+            .background(
+                RadialGradient(
+                    gradient: Gradient(colors: [Color(hex: "27124F"), Color(hex: "361C7D")]),
+                    center: .center,
+                    startRadius: 0,
+                    endRadius: 100
+                )
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+            )
+            .overlay(
+                RadialGradient(
+                    gradient: Gradient(colors: [Color.white.opacity(0.2), Color.clear]),
+                    center: .topLeading,
+                    startRadius: 0,
+                    endRadius: 50
+                )
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+            )
+            .overlay(
+                RadialGradient(
+                    gradient: Gradient(colors: [Color.clear, Color.white.opacity(0.1)]),
+                    center: .bottomTrailing,
+                    startRadius: -50,
+                    endRadius: 0
+                )
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+            )
+    }
+    
+    private func createAnswerScrollView() -> some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(spacing: 0) {
+                ForEach(0..<answerCount * 10, id: \.self) { index in
+                    Text(answers[index % answerCount])
+                        .foregroundColor(.white)
+                        .font(.custom("Helvetica-Bold", size: 35))
+                        .frame(width: 180, height: 50, alignment: .leading)
+                        .padding(.leading, 10)
+                }
+            }
+            .opacity(0.5)
+            .offset(y: offset)
+            .animation(Animation.easeInOut(duration: 4).repeatCount(0, autoreverses: false))
+        }
+    }
+    
+    private func createButton() -> some View {
+        VStack {
+            Spacer()
+            HStack {
+                if !isAnimating {
+                    Spacer()
+                    Button(action: startAnimation) {
+                        Image(systemName: "arrow.clockwise")
+                            .font(.body)
+                            .foregroundColor(.white)
+                            .padding(15)
+                            .background(Material.ultraThinMaterial.opacity(0.4))
+                            .clipShape(Circle())
+                            .overlay(
+                                Circle()
+                                    .stroke(Color.white.opacity(0.3), lineWidth: 0.2)
+                            )
+                            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 5)
+                    }
+                    .padding(10)
+                    .clipShape(Circle())
+                    .animation(.easeInOut, value: isAnimating)
+                }
             }
         }
     }
@@ -131,6 +146,8 @@ struct Magic8CubeView: View {
         showParticles = true
         answerOpacity = 0.0
         particleManager.resetParticles()
+        shakeCube()
+
         offset = -CGFloat(selectedAnswerIndex * 50)
             DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                 selectedAnswerIndex = Int.random(in: 0..<answers.count)
@@ -143,6 +160,17 @@ struct Magic8CubeView: View {
                 }
             }
     }
+    
+    private func shakeCube() {
+        let animation = Animation.easeInOut(duration: 0.1).repeatCount(10, autoreverses: true)
+        withAnimation(animation) {
+            shakeOffset = 10
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            shakeOffset = 0
+        }
+        particleManager.shakeParticles()
+    }
 }
 
 struct Magic8CubeView_Previews: PreviewProvider {
@@ -150,12 +178,6 @@ struct Magic8CubeView_Previews: PreviewProvider {
         Magic8CubeView()
     }
 }
-
-
-
-
-
-
 
 struct CustomParticleView: View {
     @EnvironmentObject var particleManager: ParticleManager
@@ -177,27 +199,31 @@ struct CustomParticleView: View {
     }
 }
 
-
 class ParticleManager: ObservableObject {
     @Published var particles = [Particle]()
-    
+
     init() {
         createParticles()
     }
-    
-    func createParticles() {
+
+    func createParticles(sizeRange: ClosedRange<CGFloat> = 0.2...0.9, lifetimeRange: ClosedRange<Double> = 1...2, ensureDistanceFromTextCenter: Bool = false) {
         particles.removeAll()
-        for _ in 0..<4000 {
-            let size = CGFloat.random(in: 0.2...0.9)
-            let lifetime = Double.random(in: 1...2)
-            let position = CGPoint(x: CGFloat.random(in: 0...200), y: CGFloat.random(in: 0...200))
+        for _ in 0..<2000 {
+            let size = CGFloat.random(in: sizeRange)
+            let lifetime = Double.random(in: lifetimeRange)
+            var position: CGPoint
+
+            repeat {
+                position = CGPoint(x: CGFloat.random(in: 0...200), y: CGFloat.random(in: 0...200))
+            } while ensureDistanceFromTextCenter && distanceToTextCenter(position) < 40 // Ensure fewer particles near the text center if needed
+
             let opacity = Double.random(in: 0.5...1.0)
             let particle = Particle(size: size, lifetime: lifetime, position: position, initialPosition: position, opacity: opacity)
-            
+
             particles.append(particle)
         }
     }
-    
+
     func moveParticlesAwayFromTextCenter() {
         for index in particles.indices {
             let newPosition = calculateNewPosition(for: particles[index].initialPosition)
@@ -205,39 +231,21 @@ class ParticleManager: ObservableObject {
             particles[index].lifetime = Double.random(in: 1...2)
         }
     }
-    
+
     func resetParticles() {
         for index in particles.indices {
             particles[index].position = particles[index].initialPosition
             particles[index].opacity = Double.random(in: 0.1...1.0)
+        }
+    }
 
-        }
-    }
-    
-    func shimmerParticles() {
+    func shakeParticles() {
         for index in particles.indices {
-            particles[index].opacity = Double.random(in: 0.1...1.0)
+            particles[index].position.x += CGFloat.random(in: -10...10)
+            particles[index].position.y += CGFloat.random(in: -10...10)
         }
     }
-    
-    func reduceParticlesNearTextCenter() {
-        particles.removeAll()
-        for _ in 0..<4000 {
-            let size = CGFloat.random(in: 0.1...0.6)
-            let lifetime = Double.random(in: 1...2)
-            var position: CGPoint
-            
-            repeat {
-                position = CGPoint(x: CGFloat.random(in: 0...200), y: CGFloat.random(in: 0...200))
-            } while distanceToTextCenter(position) < 40 // Ensure fewer particles near the text center
-            
-            let opacity = Double.random(in: 0.5...1.0)
-            let particle = Particle(size: size, lifetime: lifetime, position: position, initialPosition: position, opacity: opacity)
-            
-            particles.append(particle)
-        }
-    }
-    
+
     private func calculateNewPosition(for position: CGPoint) -> CGPoint {
         let textCenter = CGPoint(x: 70, y: 100) // Adjusted for text alignment
         let dx = position.x - textCenter.x
@@ -249,12 +257,12 @@ class ParticleManager: ObservableObject {
         let spreadY = dy > 0 ? CGFloat.random(in: 1.0...1.5) : CGFloat.random(in: 0.5...1.0)
         return CGPoint(x: max(-20, min(220, newX * spreadX)), y: max(-20, min(220, newY * spreadY))) // Ensure particles stay within bounds
     }
-    
+
     private func distanceToTextCenter(_ position: CGPoint) -> CGFloat {
         let textCenter = CGPoint(x: 70, y: 100) // Adjusted for text alignment
         let dx = position.x - textCenter.x
         let dy = position.y - textCenter.y
-        return sqrt(dx*dx + dy*dy)
+        return sqrt(dx * dx + dy * dy)
     }
 }
 
@@ -266,7 +274,6 @@ struct Particle: Identifiable {
     var initialPosition: CGPoint
     var opacity: Double
 }
-
 
 extension Color {
     init(hex: String) {
